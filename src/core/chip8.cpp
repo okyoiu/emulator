49b079ab991644de8984a1f256428feb6af97788
okyoiu <cesar.sanc219@gmail.com>
2026-08-02 16:37:43 -0500

    feat: LD Vx, K, which means that we wait for a key press then store it inside Vx

--- src/core/chip8.cpp
+++ src/core/chip8.cpp
@@ -468,3 +468,81 @@ void Chip8::OP_Fx07()
 
 	registers[Vx] = delayTimer;
 }
+
+// key press (wait) then store
+void Chip8::OP_Fx0A()
+{
+	// this function DOESNT know which key it cares about, so we check everything
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	if (keypad[0])
+	{
+		registers[Vx] = 0;
+	}
+	else if (keypad[1])
+	{
+		registers[Vx] = 1;
+	}
+	else if (keypad[2])
+	{
+		registers[Vx] = 2;
+	}
+	else if (keypad[3])
+	{
+		registers[Vx] = 3;
+	}
+	else if (keypad[4])
+	{
+		registers[Vx] = 4;
+	}
+	else if (keypad[5])
+	{
+		registers[Vx] = 5;
+	}
+	else if (keypad[6])
+	{
+		registers[Vx] = 6;
+	}
+	else if (keypad[7])
+	{
+		registers[Vx] = 7;
+	}
+	else if (keypad[8])
+	{
+		registers[Vx] = 8;
+	}
+	else if (keypad[9])
+	{
+		registers[Vx] = 9;
+	}
+	else if (keypad[10])
+	{
+		registers[Vx] = 10;
+	}
+	else if (keypad[11])
+	{
+		registers[Vx] = 11;
+	}
+	else if (keypad[12])
+	{
+		registers[Vx] = 12;
+	}
+	else if (keypad[13])
+	{
+		registers[Vx] = 13;
+	}
+	else if (keypad[14])
+	{
+		registers[Vx] = 14;
+	}
+	else if (keypad[15])
+	{
+		registers[Vx] = 15;
+	}
+	else
+	{
+		// here we de-increment and Cycle() will increment (so cancelling one another)
+		// this will then continue until a key is pressed
+		pc -= 2;
+	}
+}
